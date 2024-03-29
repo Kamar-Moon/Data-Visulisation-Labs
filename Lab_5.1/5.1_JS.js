3362a774294f12b59e3270a7bb713f5e9577228e
Kamar-Moon <sapphirewolf987@outlook.com>
2024-03-29 15:44:36 +1100

    5.1 add button

--- Lab_5.1/5.1_JS.js
+++ Lab_5.1/5.1_JS.js
@@ -1,27 +1,28 @@
 function init(){
-            //button to update data
-        
-            
-
             var w = 500;
             var h = 200;
             margine = 20
     
-            var dataset = [15, 25, 25, 8 , 20, 27, 28]      
+            var dataset = [15, 25, 25, 8 , 20, 27, 28]
+            
+            
+
+               
     
+            //scale x axis
             var xScale = d3.scaleBand() //scale for ordinal data
                             .domain(d3.range(dataset.length)) //calculate range of domain
                             .rangeRound([0,w]) //range of domain from 0 to width of svg
                             .paddingInner([0.05]); //padding 5% of bandwidth
     
-    
+            //Scale y axis
             var yScale = d3.scaleBand()
                             .domain(d3.range(dataset.length))
                             .rangeRound([0, d3.max(dataset, function(d){
                                 return d[0];
                             })]);
     
-    
+            //add svg canvas
             var svg = d3.select("body")
                         .append("svg")
                         .attr("width", w)
@@ -44,25 +45,26 @@ function init(){
                 })
                 .classed("bar", true);            
               
-    
-            // Add y-axis label
-            /*svg.append("text")
-                .attr("class", "axis-label")
-                .attr("transform", "rotate(-90)")
-                .attr("x", -h / 2)
-                .attr("y", 15)
-                .attr("text-anchor", "middle")
-                .text("Energy %")
-                .classed("axis-label", true);
-    
-            // Add graph title
-            svg.append("text")
-                .attr("class", "graph-title")
-                .attr("x", w / 2)
-                .attr("y", 20)
-                .attr("text-anchor", "middle")
-                .text("Energy Levels Throughout Week")
-                .classed("graph-title", true);   */
+            // On button click update with new data
+            d3.select("#update")
+            .on("click", function() {
+                
+                //new values for dataset
+                var dataset = [11, 12, 15, 20, 18, 17, 23, 25, 5, 21, 22, 25];
 
+                //Update all rects
+                svg.selectAll("rect")
+                    .data(dataset)
+                    .attr("y", function(d) {
+                        return h - yScale(d);
+                        console.log("value of y for d = " + d + ": " + y);
+                        return y;
+                    })
+                    .attr("height", function(d){
+                        return yScale(d);
+                    });
+            });
+    
+        
 }
 window.onload=init;
