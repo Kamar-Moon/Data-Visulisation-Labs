2af8fdba180cb7b4dec61c8999eb502fd63d0080
Kamar-Moon <sapphirewolf987@outlook.com>
2024-04-01 15:47:01 +1100

    task 5.2 finished

--- Lab_5.2/5.2_JS.js
+++ Lab_5.2/5.2_JS.js
@@ -1,129 +1,230 @@
-function init(){
+function init() {
     var w = 600;
     var h = 250;
-   
+    var padding = 50;
 
-    var dataset = [15, 25, 24, 8 , 20, 27, 28, 6, 9, 10, 22, 
-        24, 13, 18, 19, 21, 32, 30, 36, 11, 33, 37, 32, 12, 17]
-    
-    
-    //scale x axis
-    var xScale = d3.scaleBand() //scale for ordinal data
-                    .domain(d3.range(dataset.length)) //calculate range of domain
-                    .rangeRound([0,w]) // round values to whole number, range of domain from 0 to width of svg
-                    .paddingInner([0.05]); //padding 5% of bandwidth
+    var dataset = [
+        15, 25, 24, 8, 20, 22, 28, 6, 9, 10, 22, 
+        24, 13, 18, 19, 21, 16, 25, 24, 11, 33, 23, 19, 12, 17
+    ];
 
-    //Scale y axis - this needs to use linear scale
+    // Scale x axis
+    var xScale = d3.scaleBand() // Scale for ordinal data
+                    .domain(d3.range(dataset.length)) // Calculate range of domain
+                    .rangeRound([0, w]) // Round values to whole number, range of domain from 0 to width of svg
+                    .paddingInner([0.05]); // Padding 5% of bandwidth
+
+    // Scale y axis - this needs to use linear scale
     var yScale = d3.scaleLinear() 
-                .domain([d3.min(dataset), d3.max(dataset)])
-                .rangeRound([h, 0]); //avalible space is height of canvas to 0 
-    
-//add svg canvas
+                    .domain([0, d3.max(dataset)])
+                    .rangeRound([h, 0]); // Available space is height of canvas to 0
+
+    // Add svg canvas
     var svg = d3.select("body")
                 .append("svg")
                 .attr("width", w)
                 .attr("height", h);
 
-    //Add Bars
+
+    // Add Bars
     svg.selectAll("rect")
         .data(dataset)
         .enter()
         .append("rect")
-        .attr("x",  function(d, i){
+        .attr("x", function(d, i) {
             return xScale(i);
         })
-        .attr("y", function(d){
-            return h - (d * 4) - 20 // Adjust the y position to the bottom of the SVG minus the height of each bar
+        .attr("y", function(d) {
+            return h - (d * 4); // Adjust the y position to the bottom of the SVG minus the height of each bar
         })
-        .attr("width", xScale.bandwidth() ) // width of bars / 5 data points - padding
-        .attr("height", function(d){
+        .attr("width", xScale.bandwidth()) // Width of bars / 5 data points - padding
+        .attr("height", function(d) {
             return d * 4;
         })
-        .attr("fill", function(d) { //dynamic fill for bars higher numbers = darker colour
-            return "rgb(0, 0, " + Math.round(d * 10) + ")" });
+        .attr("fill", function(d) { // Dynamic fill for bars higher numbers = darker colour
+            return "rgb(0, 0, " + Math.round(d * 10) + ")";
+        });
     
-    // labels for bars        
+    // Labels for bars        
     svg.selectAll("text")
         .data(dataset)
         .enter()
         .append("text")
-        .text(function(d){ return d; })
-        .attr("x", function(d, i){ return xScale(i) + xScale.bandwidth() /2; })
-        .attr("y", function(d) { return yScale(d); })
+        .text(function(d) {
+            return d;
+        })
+        .attr("x", function(d, i) {
+            return xScale(i) + xScale.bandwidth() / 2;
+        })
+        .attr("y", function(d) {
+            return h - (d * 4) + 16;
+        })
         .attr("font-family", "sans-serif")
         .attr("font-size", "11px")
         .attr("fill", "white")
         .attr("text-anchor", "middle");
 
-      
-    // On button click update with new data
-    d3.select("button")
-    .on("click", function() {
+    // Regular Update code
+    d3.select("#updatePlain")
+        .on("click", function() {
+            var NumValues = dataset.length; //number of data values to generate = length of original dataset
+            var maxValue = 25; // the largest number that can be added
+            dataset = [];
 
-        var NumValues = dataset.length;
-        var maxValue = 25;
-        dataset = [];
+            for (var i = 0; i < NumValues; i++) {
+                var newNumber = Math.floor(Math.random() * maxValue); // Math.random generates random number between 0-1, multiply that by maxValue, round to nearest integer with .floor 
+                dataset.push(newNumber);
+            }
 
-        for (var i = 0; i < NumValues; i++){
-            var newNumber = Math.floor(Math.random()* maxValue); //Math.random generates random number between 0-1, multiply that by maxValue, round to nearest integer with .floor 
-            dataset.push(newNumber);
-        }
-
-    //Update all rects
-    svg.selectAll("rect")
-        .data(dataset)
-        .join("rect")
-        .transition() //<----new transition line
-        .delay(function(d, i){
-            return i * 100;
-        })
-        .duration(2000)
-        .ease(d3.easeElasticOut)
-        .attr("x", function(d, i){
-             return xScale(i);
-            })
-        .attr("y", function(d) {
-                return h - yScale(d);
-            })
-            .attr("width", xScale.bandwidth())
-        .attr("height", function(d){
-            return yScale(d);
-            })
-        .attr("fill", function(d) { //dynamic fill for bars higher numbers = darker colour
-            return "rgb(0, 0, " + Math.round(d * 10) + ")" });
+            // Update all rects
+            svg.selectAll("rect")
+                .data(dataset)
+                .join("rect")
+                .attr("x", function(d, i) {
+                    return xScale(i);
+                })
+                .attr("y", function(d) {
+                    return h - yScale(d);
+                })
+                .attr("width", xScale.bandwidth())
+                .attr("height", function(d) {
+                    return yScale(d);
+                })
+                .attr("fill", function(d) { // Dynamic fill for bars higher numbers = darker colour
+                    return "rgb(0, 0, " + Math.round(d * 10) + ")";
+                });
     
-    //Update labels
-    svg.selectAll("text")
-        .data(dataset)
-        .join("text")
-        .transition() //<----new transition line
-        .delay(function(d, i){
-            return i * 100;
-        })
-        .duration(2000)
-        .ease(d3.easeElasticOut)
-        .text(function(d) {
-            return d;
-            })
-            .attr("x", function(d, i) {
-            return xScale(i) + xScale.bandwidth() / 2;
-            })
-            .attr("y", function(d) {return h - yScale(d) + 16;})
-        
-       
+            // Update labels
+            svg.selectAll("text")
+                .data(dataset)
+                .join("text")
+                .text(function(d) {
+                    return d;
+                })
+                .attr("x", function(d, i) {
+                    return xScale(i) + xScale.bandwidth() / 2;
+                })
+                .attr("y", function(d) {
+                    return h - yScale(d) + 16;
+                });
+        });
 
+     // Transition 1 code
+     d3.select("#tran1")
+     .on("click", function() {
+         var NumValues = dataset.length; //number of data values to generate = length of original dataset
+         var maxValue = 25; // the largest number that can be added
+         dataset = [];
 
+         for (var i = 0; i < NumValues; i++) {
+             var newNumber = Math.floor(Math.random() * maxValue); // Math.random generates random number between 0-1, multiply that by maxValue, round to nearest integer with .floor 
+             dataset.push(newNumber);
+         }
 
+         // Update all rects
+         svg.selectAll("rect")
+             .data(dataset)
+             .join("rect")
+             .transition() //<----new transition line
+             .delay(function(d, i) {
+                 return i * 100;
+             })
+             .duration(2000)
+             .ease(d3.easeElasticOut)
+             .attr("x", function(d, i) {
+                 return xScale(i);
+             })
+             .attr("y", function(d) {
+                 return h - yScale(d);
+             })
+             .attr("width", xScale.bandwidth())
+             .attr("height", function(d) {
+                 return yScale(d);
+             })
+             .attr("fill", function(d) { // Dynamic fill for bars higher numbers = darker colour
+                 return "rgb(0, 0, " + Math.round(d * 10) + ")";
+             });
+ 
+         // Update labels
+         svg.selectAll("text")
+             .data(dataset)
+             .join("text")
+             .transition() //<----new transition line
+             .delay(function(d, i) {
+                 return i * 100;
+             })
+             .duration(2000)
+             .ease(d3.easeElasticOut)
+             .text(function(d) {
+                 return d;
+             })
+             .attr("x", function(d, i) {
+                 return xScale(i) + xScale.bandwidth() / 2;
+             })
+             .attr("y", function(d) {
+                 return h - yScale(d) + 16;
+             });
+     });  
+     
+     // Transition 2 code
+     d3.select("#tran2")
+     .on("click", function() {
+         var NumValues = dataset.length; //number of data values to generate = length of original dataset
+         var maxValue = 25; // the largest number that can be added
+         dataset = [];
 
-                         
+         for (var i = 0; i < NumValues; i++) {
+             var newNumber = Math.floor(Math.random() * maxValue); // Math.random generates random number between 0-1, multiply that by maxValue, round to nearest integer with .floor 
+             dataset.push(newNumber);
+         }
 
-    })
+         // Update all rects
+         svg.selectAll("rect")
+             .data(dataset)
+             .join("rect")
+             .transition() //<----new transition line
+             .delay(function(d, i) {
+                 return i * 100;
+             })
+             .duration(2000)
+             .ease(d3.easeCircleIn)
+             .attr("x", function(d, i) {
+                 return xScale(i);
+             })
+             .attr("y", function(d) {
+                 return h - yScale(d);
+             })
+             .attr("width", xScale.bandwidth())
+             .attr("height", function(d) {
+                 return yScale(d);
+             })
+             .attr("fill", function(d) { // Dynamic fill for bars higher numbers = darker colour
+                 return "rgb(0, 0, " + Math.round(d * 10) + ")";
+             })
+             .on("end", function(){ // Transition for labels starts after bars transition ends
+                // Update labels
+                    svg.selectAll("text")
+                    .data(dataset)
+                    .join("text")
+                    .transition() //<----new transition line
+                    .delay(function(d, i) {
+                        return i * 100;
+                    })
+                    .duration(1000)
+                    .ease(d3.easeCircleIn)
+                    .text(function(d) {
+                        return d;
+                    })
+                    .attr("x", function(d, i) {
+                        return xScale(i) + xScale.bandwidth() / 2;
+                    })
+                    .attr("y", function(d) {
+                        return h - yScale(d) + 16;
+                    });
 
-   
-
-
-    
-
-
+             });
+ 
+         
+     }); 
 }
-window.onload=init;
+window.onload = init;
