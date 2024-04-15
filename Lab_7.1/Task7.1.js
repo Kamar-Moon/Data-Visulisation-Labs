01925e99a3acb89b8a0b83a05124524025853268
Kamar-Moon <sapphirewolf987@outlook.com>
2024-04-15 15:44:06 +1000

    task 7.1 progress
    still getting errors

--- Lab_7.1/Task7.1.js
+++ Lab_7.1/Task7.1.js
@@ -6,31 +6,37 @@ function init(){
 
     var dataset;
 
+    var rowCoverter = function(d){
+        return{
+            //Make new data object to combine year and month
+            // the + forces the d.year and d.month to be typed as numbers not strings
+            date: new Date(+d.year, (+d.month - 1)), //must be -1 becuase javascript month starts at 0
+            //convert number from string to int
+            number: parseInt(+d.number)
+        };
+    }
+
 
     //when running locally i have used gist, on mercury i just use the file name
-    d3.csv("Unemployment_78-95", function(data){
-        return {
+    d3.csv("Unemployment_78-95.csv", rowCoverter, function(d){
+        var dataset = data;
 
-            date: new Date(+d.year, +d.month-1),
-            number: +d.number
-        };
+        //print data to console as table
+
     }).then(function(data){
-        dataset = data;
-
+        dataset = data; // for troubleshooting data type
+        console.log(dataset); //display the resulting table of data
         LineChart(dataset);
         console.table(dataset, ["date", "number"]);
     })
 
+    
+
     function LineChart(){
-        var svg = d3.select("#chart")
+        var svg = d3.select("body")
                 .append("svg")
                 .attr("width", w)
                 .attr("height", h);
-
-            svg.append("path")
-                .datum(dataset)
-                .attr("class", "line")
-                .attr("d", line);
 
         xScale = d3.scaleTime()
                     .domain([
@@ -45,8 +51,13 @@ function init(){
                     .range(h, 0); // on y axis use the space starting from 0 to total height
 
         line = d3.line()
-                 .x(function(d) {return xScale(d.date);})
-                 .t(function(d) {return yScale(d.number);});
+                .x(function(d) {return xScale(d.date);})
+                .y(function(d) {return yScale(d.number);});
+
+        svg.append("path") // must put code here to i do not get "line not defined error"
+            .datum(dataset)
+            .attr("class", "line")
+            .attr("d", line);
     };
 
 
